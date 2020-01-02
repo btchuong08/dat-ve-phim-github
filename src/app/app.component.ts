591a60af5b745f56f066728ba953a5e33118a812
btchuong08 <53364595+btchuong08@users.noreply.github.com>
2020-01-02 14:00:41 +0700

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import Scrollbar from 'smooth-scrollbar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'datvephim';
+
+
+  ngOnInit() {
+
+    Scrollbar.initAll();
+ 
+
+  }
 }
--- src/app/home-templates/home-templates.component.ts
+++ src/app/home-templates/home-templates.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Scrollbar from 'smooth-scrollbar';
 @Component({
   selector: 'app-home-templates',
   templateUrl: './home-templates.component.html',
@@ -10,6 +10,8 @@ export class HomeTemplatesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Scrollbar.initAll();
   }
+
 
 }
